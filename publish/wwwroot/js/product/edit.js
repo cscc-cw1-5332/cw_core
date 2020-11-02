c924b7561769b4770b88b4c8533a5952cfe15ffd
Shokhrukh Erkinov <exceeditdeveloper@gmail.com>
2020-11-02 12:05:43 +0500

    Initial commit

---
+++ publish/wwwroot/js/product/edit.js
@@ -0,0 +1,84 @@
+﻿let productDialog;
+let isUnique;
+
+function editProduct(url) {
+    var formDiv = $('<div/>');
+    $.get(url)
+        .done(function (response) {
+            formDiv.html(response);
+            productDialog = formDiv.dialog({
+                autoOpen: true,
+                resizeable: false,
+                width: 600,
+                height: 400,
+                modal: true,
+                title: 'Edit product type',
+                close: function () {
+                    productDialog.dialog('destroy').remove();
+                }
+            });
+
+            $("#closeDialogBtn").click(function () {
+                productDialog.dialog('destroy').remove();
+            });
+        });
+}
+
+function submitProduct(form) {
+    $.validator.unobtrusive.parse(form);
+    var data = $(form).serializeJSON();
+    data = JSON.stringify(data);
+
+    if ($(form).valid()) {
+        $.ajax({
+            type: "POST",
+            url: "https://localhost:44313/api/products",
+            contentType: "application/json; charset=utf-8",
+            data: data,
+            success: function (data) {
+                if (data.success) {
+                    productDialog.dialog('close');
+                    toastr.success(data.message);
+                    products.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        }).fail(function (data) {
+            console.log(data);
+        });
+    }
+
+    else
+        console.log("Cannot post data!");
+
+    return false;
+}
+
+
+function deleteProduct(url) {
+    swal({
+        title: "Continue?",
+        text: "Surely sure to delete?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        products.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
---
+++ publish/wwwroot/js/product/list.js
@@ -0,0 +1,44 @@
+﻿let products;
+
+$(document).ready(() => {
+    loadProducts();
+
+    $('#editProductBtn').click(function () {
+        editProduct("/Product/Edit");
+    });
+});
+
+function loadProducts() {
+    products = $('#products')
+        .DataTable({
+            "ajax": {
+                "url": "https://localhost:44313/api/products",
+                "type": "get",
+                "datatype": "json"
+            },
+            "columns": [
+                { "data": "title", "width": "15%" },
+                { "data": "identifier", "width": "15%" },
+                { "data": "description", "width": "50%" },
+                {
+                    "data": "id",
+                    "render": function (id) {
+                        return `<div class="row">
+                                    <a  class='btn btn-success text-white'
+                                        onclick="editProduct('/Product/Edit/${id}')"
+                                        style='cursor:pointer;'>
+                                        <i class="fa fa-edit"></i> Edit
+                                    </a>
+                                    &nbsp;
+                                    <a  class='btn btn-danger text-white'
+                                        onclick="deleteProduct('https://localhost:44313/api/products?id=${id}')"
+                                        style='cursor:pointer;'>
+                                        <i class="fa fa-trash"></i> Delete
+                                    </a>
+                                </div>`
+                    },
+                    "width": "20%"
+                }
+            ]
+        });
+}
